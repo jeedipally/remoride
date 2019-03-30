b222f8fbb347fe0098360bccba3d2f39a0c54be4
unknown <jeedipally@gmaail.com>
2019-03-30 03:12:03 -0700

    ified:   src/main/resources/data.sql

--- SpringBoot_Hacktrix/src/main/resources/data.sql
+++ SpringBoot_Hacktrix/src/main/resources/data.sql
@@ -2,7 +2,9 @@ INSERT INTO loan (loanId, name, loanType, loanAmount, loanStatus) VALUES (1, 'PR
 INSERT INTO loan (loanId, name, loanType, loanAmount, loanStatus) VALUES (2, 'NARENDAR', 'Personal Loan','30000', 'Active');
 INSERT INTO loan (loanId, name, loanType, loanAmount, loanStatus) VALUES (3, 'Mohammad', 'HOME LOAN','30000', 'inActive');
 INSERT INTO loan (loanId, name, loanType, loanAmount, loanStatus) VALUES (4, 'Naresh', 'Credit Card','30000', 'Active');
-
+INSERT INTO loan (loanId, name, loanType, loanAmount, loanStatus) VALUES (5, 'jj', 'Credit Card','10000', 'Active');
+INSERT INTO loan (loanId, name, loanType, loanAmount, loanStatus) VALUES (6, 'tt', 'Credit Card','5000', 'Active');
+INSERT INTO loan (loanId, name, loanType, loanAmount, loanStatus) VALUES (7, 'tt', 'Credit Card','3000', 'Active');
 
 INSERT INTO customer (id, loanId, name, salaryType, loan, rentedhouse, salAmount) VALUES (1, '1', 'ABCDE','Salaried', '10000', 'Rented', '100000');
 

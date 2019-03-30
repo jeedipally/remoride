42f9f465bb21fc21d0d16f3f831cad5a6fe7b719
mohammedrafi018@gmail.com <mohammedrafi018@gmail.com>
2019-03-30 11:17:27 +0530

    adding angilar project

---
+++ RemoRides/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoanFormComponent } from './loan-form/loan-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    LoanFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
